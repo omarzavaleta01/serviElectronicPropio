bf8d7c8528e16b64fbb91baf19286319207cb988
axelandrio <axel_ayalac@outlook.com>
2022-06-28 20:35:35 -0500

    Se agrega js

---
+++ resurces/js/perfil.js
@@ -0,0 +1,55 @@
+
+const cerrarSes=document.querySelector('.cerrar');
+const userUp=document.querySelector('.up');
+const btServ=document.querySelector('.slServ');
+let text="Cerrar sesión"
+const perfil= auth.onAuthStateChanged;
+const decora= document.querySelector(".login");
+const icoDeco=document.querySelector(".logico")
+
+
+cerrarSes.addEventListener('click' ,e=>{
+
+    auth
+    .signOut()
+    .then(()=>{
+        alert("sesion cerrada")
+        window.location.href = "login.html";
+    })
+    idTiEsa = setTimeout(mensajes, 2000);
+})
+
+auth.onAuthStateChanged(user =>{
+    if(user){
+
+       let disName=user.email;
+       userUp.textContent = disName;
+       cerrarSes.textContent=text;  
+       //alert("Bienvenido " +  user.email) 
+       decoracion();
+    }else{
+        cerrarSes.classList.toggle("loginClose-active");       
+    }
+})
+
+function mensajes(){
+    alert("Sesión cerrada")
+}
+function decoracion(){
+    decora.classList.toggle("login-active");
+    icoDeco.classList.toggle("logico-active");
+}
+
+btServ.addEventListener("click", e =>{
+
+    auth.onAuthStateChanged(user =>{
+        if(user){
+            window.location.href = "form-solicit-serv.html";
+        }else{
+
+            window.location.href = "login.html";
+            alert("Para poder solicitar el servicio deves antes iniciar sesión");
+        }
+})
+
+});
